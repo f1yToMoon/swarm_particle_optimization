9c67cee818b4951f966613fe072c89dd16d2fd81
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2023-05-01 11:49:07 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,10 +14,32 @@ float f(float x1, float x2) {
     return res;
 }
 
+void printGraph() {
+    std::vector<std::vector<double>> x, y, z;
+    for (double i = -100; i <= 100;  i += 0.25) {
+        std::vector<double> x_row, y_row, z_row;
+        for (double j = -100; j <= 100; j += 0.25) {
+            x_row.push_back(i);
+            y_row.push_back(j);
+            z_row.push_back(f(i, j));
+        }
+        x.push_back(x_row);
+        y.push_back(y_row);
+        z.push_back(z_row);
+    }
+
+    plt::plot_surface(x, y, z);
+    plt::save("/home/nick/2.png");
+    plt::cla();
+    plt::clf();
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    printGraph();
+
     srand (static_cast <unsigned> (time(0)));
 
     ui->setupUi(this);
